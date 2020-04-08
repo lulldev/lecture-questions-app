97548c2d0d9be1413b1a4860898e599a024669c7
Ivan Kalagin <ikalagin@onlinetours.ru>
2020-04-08 20:24:55 +0300

    Some changes

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { routes } from 'startup/routes';
 
-import 'startup/client';
+// import 'startup/client';
 
 Meteor.startup(() => {
   render(routes(), document.getElementById('root'));
